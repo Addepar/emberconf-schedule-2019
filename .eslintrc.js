56a91c592a428f049ade502a06e596fba05e11db
Kevin Pfefferle <kevin@pfefferle.co>
2018-02-09 16:32:02 -0500

    Mark serve.js as node file for ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,8 @@ module.exports = {
         'testem.js',
         'ember-cli-build.js',
         'config/**/*.js',
-        'lib/*/index.js'
+        'lib/*/index.js',
+        'serve.js'
       ],
       parserOptions: {
         sourceType: 'script',
