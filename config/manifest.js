3d71cb6ac42201288b9d3f12c045ef6c5e7fc671
Kevin Pfefferle <kevin@pfefferle.co>
2018-02-09 15:35:37 -0500

    Add web app manifest

---
+++ config/manifest.js
@@ -0,0 +1,50 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://github.com/san650/ember-web-app#documentation for a list of
+  // supported properties
+
+  return {
+    name: "EmberConf 2018",
+    short_name: "EmberConf",
+    description: "EmberConf 2018 Schedule",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#e8edf1",
+    theme_color: "#e15d44",
+    icons: [
+      {
+        src: '/icons/192.png',
+        sizes: '192x192'
+      },
+      {
+        src: '/icons/512.png',
+        sizes: '512x512'
+      },
+      {
+        src: '/icons/280.png',
+        sizes: '280x280',
+        targets: ['apple']  // non-standard property
+      },
+      {
+        src: '/icons/favicon.png',
+        sizes: '32x32',
+        targets: ['favicon']
+      },
+      {
+        src: '/icons/70.png',
+        element: 'square70x70logo', // non-standard property
+        targets: ['ms'] // non-standard-property
+      },
+      {
+        src: '/icons/logo-mono.svg',
+        safariPinnedTabColor: '#e15d44', // non-standard property
+        targets: ['safari-pinned-tab'] // non-standard-property
+      }
+    ],
+    ms: {
+      tileColor: '#001d36'
+    }
+  };
+}
