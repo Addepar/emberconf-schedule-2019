92d704e0b4a95caa7ac6ae3b267e60543a4d4ff6
Kevin Pfefferle <kevin@pfefferle.co>
2018-02-09 14:12:26 -0500

    Add service worker and cache assets for offline load

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,15 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'asset-cache': {
+      include: [
+        'assets/**/*'
+      ]
+    },
+    'esw-cache-fallback': {
+      patterns: [ '/' ],
+      version: '1' // Changing the version will bust the cache
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
