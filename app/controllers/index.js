202038409c6ebafae190bf14733f697dcb7832c3
Kevin Pfefferle <kevin@pfefferle.co>
2018-03-02 18:26:38 -0500

    Scroll schedule to show current/upcoming sessions

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,12 @@
 import Controller from '@ember/controller';
 import { alias } from '@ember/object/computed';
 import { later } from '@ember/runloop';
+import { inject as service } from '@ember/service';
 import moment from 'moment';
 
 export default Controller.extend({
+  fastboot: service(),
+
   days: alias('model'),
 
   now: null,
@@ -12,6 +15,8 @@ export default Controller.extend({
     // FIXME: Force Day 1 date with local clock time for testing
     let localTime = moment().format('HH:mm:ss');
     this.set('now', moment(`2018-03-13T${localTime}-07:00`).format());
+
+    if (this.get('fastboot.isFastBoot')) { return; }
 
     later(this, function() {
       this._setNow();
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,22 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 import Schedule from 'emberconf/lib/data';
 
 export default Route.extend({
+  fastboot: service(),
+  scheduler: service(),
+
   model() {
     return Schedule;
+  },
+
+  afterModel() {
+    if (this.get('fastboot.isFastBoot')) { return; }
+
+    this.get('scheduler').scheduleWork('afterContentPaint', () => {
+      // Scroll second-to-last finished session to top to ensure upcoming sessions are visible
+      let pastSessions = document.getElementsByClassName('is-past');
+      pastSessions[pastSessions.length-2].scrollIntoView(true);
+    })
   }
 });
