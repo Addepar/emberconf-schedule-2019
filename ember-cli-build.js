c00571385624015bb3d3eca5ca370206dabc410f
Kevin Pfefferle <kevin@pfefferle.co>
2018-03-12 10:43:48 -0700

    Revert "Revert "Add wasm-integration""
    This reverts commit 5ab9c5003447dded8519fc231db9de6972e53631.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -44,6 +44,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('vendor/something-hacky-this-way-comes.js', { prepend: true });
 
   return app.toTree();
 };
