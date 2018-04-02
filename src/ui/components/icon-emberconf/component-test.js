8dbffd9fe93a3f1b820c362b0574352466ed3c97
Isaac Ezer <isaac.ezer@gmail.com>
2018-04-02 13:06:24 -0400

    Component integration tests

---
+++ src/ui/components/icon-emberconf/component-test.js
@@ -0,0 +1,16 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, find } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | icon-emberconf', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders an svg', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{icon-emberconf}}`);
+    assert.ok(find('svg'));
+  });
+});
---
+++ src/ui/routes/application/-components/footer-prompt/component-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | footer-prompt', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{footer-prompt}}`);
+
+    assert.equal(this.element.querySelector('p').innerText, "Curious what's going on here?");
+  });
+});
