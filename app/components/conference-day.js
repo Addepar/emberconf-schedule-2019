e542e0eee8492b9ec594abfb530d46613d152724
Kevin Pfefferle <kevin.pfefferle@addepar.com>
2019-03-12 18:03:30 -0400

    Update component invocation to use angle bracket syntax

--- app/components/conference-day.js
+++ app/components/conference-day.js
@@ -6,7 +6,7 @@ export default Component.extend({
   day: null,
   index: 0,
 
-  title: computed('day.date', function () {
+  title: computed('day.date', 'index', function () {
     let dayNumber = this.index + 1;
     let formattedDate = moment(this.get('day.date')).format('MMMM D');
     return `Day ${dayNumber}: ${formattedDate}`;
