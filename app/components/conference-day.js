829012e8bfe950f072e1e7ee6653f8288c3647ea
Matthew Beale <matt.beale@addepar.com>
2019-03-14 16:51:44 -0400

    Use autotrack instead of CPs
    Autotrack seems to work but only if you track args.
    
    Also leave in some debug code.

--- app/components/conference-day.js
+++ app/components/conference-day.js
@@ -1,10 +1,8 @@
 import Component from '@glimmer/component';
-import { computed } from '@ember/object';
 import moment from 'moment';
 
 export default class extends Component {
 
-  @computed('args.{day.date,index}')
   get title() {
     let dayNumber = this.args.index + 1;
     let formattedDate = moment(this.args.day.date).format('MMMM D');
