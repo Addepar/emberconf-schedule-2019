4676271189f40fdf32c04a238a6fccc139936616
Kevin Pfefferle <kevin.pfefferle@addepar.com>
2019-03-15 15:19:30 -0400

    Refinements to debug mode

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,4 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
@@ -17,11 +16,26 @@ export default class extends Controller {
     return !this.fastboot.isFastBoot && window.location.search.includes('debug');
   }
 
+  get secondsInDay() {
+    let nowTime = moment(this.now).utcOffset(ENV.APP.UTC_OFFSET).format('H:mm:ss');
+    return moment.duration(nowTime).asSeconds();
+  }
+  set secondsInDay(seconds) {
+    // Use range slider seconds with Day 1 for debug
+    let time = moment('2019-01-01').startOf('day').seconds(seconds).format('HH:mm:ss');
+    this._setFakeDayOne(time);
+    return seconds;
+  }
+
+  _setFakeDayOne(time) {
+    this.now = `2019-03-19T${time}${ENV.APP.UTC_OFFSET}`;
+  }
+
   _setNow() {
     if (ENV.APP.shouldForceDayOne) {
       // Use local clock time with Day 1 for local development
       let time = moment().format('HH:mm:ss');
-      this.now = `2019-03-19T${time}${ENV.APP.UTC_OFFSET}`;
+      this._setFakeDayOne(time);
     } else {
       // Use real date and time for non-dev environments
       this.now = moment().utcOffset(ENV.APP.UTC_OFFSET).format();
@@ -40,10 +54,5 @@ export default class extends Controller {
   constructor() {
     super(...arguments);
     this._setNow();
-  }
-
-  @action
-  setSecondsInDay(secondsString) {
-    this.now = moment('2019-03-19').hours(0).minutes(0).seconds(secondsString).format();
   }
 }
